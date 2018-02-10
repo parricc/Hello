585a068fd8b4e12207058e95650f33969c0127f5
parricc <parricc@gmail.com>
2018-02-10 01:03:43 +0000

    changed test to fail

--- test.rb
+++ test.rb
@@ -11,7 +11,7 @@ class MyAppTest < Minitest::Test
 
   def test_my_default
     get '/'
-    assert_equal 'Hello, world!', last_response.body
+    assert_equal 'Foo, world!', last_response.body
   end
 
   def test_with_params
